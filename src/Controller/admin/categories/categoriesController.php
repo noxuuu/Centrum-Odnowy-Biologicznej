c219e28dd594d2969b7745d4e10f7a238057b966
Patryk <patryk58595@gmail.com>
2021-08-11 21:25:08 +0200

    Created category entity

--- src/Controller/admin/categories/categoriesController.php
+++ src/Controller/admin/categories/categoriesController.php
@@ -4,7 +4,9 @@
 namespace App\Controller\admin\categories;
 
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -19,15 +21,47 @@ class categoriesController extends AbstractController
      * Get common of admin
      * @Route("/adminpanel/categories", name="adminCategories")
      */
-    public function categories()
+    public function categories(Request $request)
     {
+        // get repos
+        $categoriesRepo = $this->getDoctrine()->getRepository(Category::class);
+
+        // get selected category
+        $selectedCategory = $request->query->get('category');
+        if($selectedCategory !== null)
+            $selectedCategory = $categoriesRepo->find($selectedCategory);
+        else
+            $selectedCategory = null;
+
+        // get data
+        $categories = $selectedCategory == null ? $categoriesRepo->findAll():$categoriesRepo->findBy(['parent' => $selectedCategory->getId()]);
+
+
+        // add some breadcrumbs
+        $breadcrumbs = [
+            ['Panel Administracyjny', $this->generateUrl('adminDashboard')],
+            ['Kategorie ofert', $this->generateUrl('adminCategories')]
+        ];
+
+        // Add to breadcrumbs
+        if($selectedCategory !== null) {
+            $currentParent = $selectedCategory;
+            for($i = 0; $i <= count($categories); $i++) {
+                if($currentParent->getParentCategory() === null)
+                    break;
+
+                $parentOf = $currentParent->getParentCategory();
+                array_push($breadcrumbs, [$parentOf->getName(), $this->generateUrl("adminCategories", ['category' => $parentOf->getId()])]);
+            }
+            array_push($breadcrumbs, [$selectedCategory->getName(), $this->generateUrl("adminCategories", ['category' => $selectedCategory->getId()])]);
+        }
+
         return $this->render('admin/pages/categories/index.html.twig', [
             'mainTitle' => 'Centrum odnowy biologicznej',
-            'pageTitle' => 'Kategorie ofert',
-            'breadcrumbs' => [
-                ['Panel Administracyjny', $this->generateUrl('adminDashboard')],
-                ['Kategorie ofert', $this->generateUrl('adminCategories')]
-            ]
+            'pageTitle' => $selectedCategory === null ? 'Kategorie ofert' : 'Podkategorie oferty'.$selectedCategory,
+            'breadcrumbs' => $breadcrumbs,
+            'categories' => $categories,
+            'selectedCategory' => $selectedCategory
         ]);
     }
 }
---
+++ src/Entity/Category.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $orderNumber;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="parentCategory")
+     */
+    private $parent;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Category::class, mappedBy="parent")
+     */
+    private $subcategories;
+
+    public function __construct()
+    {
+        $this->subcategories = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getOrderNumber(): ?int
+    {
+        return $this->orderNumber;
+    }
+
+    public function setOrderNumber(int $orderNumber): self
+    {
+        $this->orderNumber = $orderNumber;
+
+        return $this;
+    }
+
+    public function getParent(): ?self
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?self $parent): self
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|self[]
+     */
+    public function getSubCategories(): Collection
+    {
+        return $this->subcategories;
+    }
+
+    public function addSubCategory(self $parentCategory): self
+    {
+        if (!$this->subcategories->contains($parentCategory)) {
+            $this->subcategories[] = $parentCategory;
+            $parentCategory->setParent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSubCategory(self $parentCategory): self
+    {
+        if ($this->subcategories->removeElement($parentCategory)) {
+            // set the owning side to null (unless already changed)
+            if ($parentCategory->getParent() === $this) {
+                $parentCategory->setParent(null);
+            }
+        }
+
+        return $this;
+    }
+}
